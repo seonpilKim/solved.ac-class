442fdc1871fefd2da1e8d5f0f747cb6f13b5b238
seonpilKim <kimsunpil123@naver.com>
2021-02-17 01:29:03 +0900

    [1918] 후위 표기식

---
+++ class-4/1918/1918.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void Postfix(string);
+
+int main() {
+	string infix;
+
+	cin >> infix;
+
+	Postfix(infix);
+
+	return 0;
+}
+
+void Postfix(string infix) {
+	stack<char> s;
+
+	for (int i = 0; i < infix.size(); i++) {
+		if (infix[i] == '(') s.push(infix[i]);
+
+		else if (infix[i] == ')') {
+			while (!s.empty() && s.top() != '(') {
+				cout << s.top(); s.pop();
+			}
+			s.pop();
+		}
+
+		else if (infix[i] == '+' || infix[i] == '-') {
+			while (!s.empty() && s.top() != '(') {
+				cout << s.top(); s.pop();
+			}
+			s.push(infix[i]);
+		}
+
+		else if (infix[i] == '*' || infix[i] == '/') {
+			while (!s.empty() && ((s.top() == '*') || (s.top() == '/'))) {
+				cout << s.top(); s.pop();
+			}
+			s.push(infix[i]);
+		}
+
+		else cout << infix[i];
+	}
+
+	while (!s.empty()) {
+		cout << s.top(); s.pop();
+	}
+}
