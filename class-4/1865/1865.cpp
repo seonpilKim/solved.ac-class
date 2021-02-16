bb3174f53ae5317484e38a5e94b4ca648b691923
seonpilKim <kimsunpil123@naver.com>
2021-02-16 18:20:11 +0900

    [1865] 웜홀

--- class-4/1865/1865.cpp
+++ class-4/1865/1865.cpp
@@ -1,109 +1,73 @@
 #include <iostream>
 #include <limits.h>
-#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-#define INF INT_MAX
-
-class Edge {
-public:
-	int v2;
-	int len;
-	Edge* next;
-
-	Edge(int v2, int len) {
-		this->v2 = v2;
-		this->len = len;
-		this->next = nullptr;
-	}
-};
-
-class Vertex {
-public:
-	int len;
-	int edges;
-	Edge* head;
-	Edge* tail;
-
-	Vertex() {
-		this->len = INF;
-		this->edges = 0;
-		this->head = nullptr;
-		this->tail = nullptr;
-	}
-
-	void addEdge(int v2, int len) {
-		Edge* newEdge = new Edge(v2, len);
-		if (edges == 0) this->head = this->tail = newEdge;
-		else {
-			this->tail->next = newEdge;
-			this->tail = newEdge;
-		}
-		this->edges++;
-	}
-};
+#define INF 2000000000
+#define MAX_SIZE 501
+#define START 1
 
 int N, M, W;
 
-bool Bellman_Ford(Vertex*, int);
+bool Bellman_Ford(int*, vector<pair<int, int>>*);
 
 int main() {
 	int TC;
 	int S, E, T;
-	bool answer;
 
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
 	cin >> TC;
-	cin >> N >> M >> W;
 
 	for (int i = 0; i < TC; i++) {
-		Vertex* vertex = new Vertex[N + 1];
-		answer = false;
+		int Vertex[MAX_SIZE];
+		vector<pair<int, int>> Edge[MAX_SIZE];
+
+		cin >> N >> M >> W;
+
+		// 초기화
+		fill(Vertex, Vertex + N + 1, INF);
+		Vertex[START] = 0;
+		for (int i = 1; i <= N; i++) Edge[i].clear();
+
+		// 도로 입력
 		for (int j = 0; j < M; j++) {
-			// 도로 입력
 			cin >> S >> E >> T;
-			vertex[S].addEdge(E, T);
-			vertex[E].addEdge(S, T);
+			Edge[S].push_back(make_pair(E, T));
+			Edge[E].push_back(make_pair(S, T));
 		}
+
+		// 웜홀 입력
 		for (int j = 0; j < W; j++) {
-			// 웜홀 입력
 			cin >> S >> E >> T;
-			vertex[S].addEdge(E, -T);
+			Edge[S].push_back(make_pair(E, -T));
 		}
-		for (int start = 1; start <= N; start++) {
-			// 모든 정점을 한 번씩 시작점으로 테스팅
-			if (Bellman_Ford(vertex, start)) {
-				cout << "YES\n";
-				answer = true;
-				break;
-			}
-		}
-		if (!answer) cout << "NO\n";
+
+		// 벨만-포드 알고리즘
+		if (Bellman_Ford(Vertex, Edge)) cout << "YES\n";
+		else cout << "NO\n";
 	}
 
 	return 0;
 }
-/*
-	N : 지점 개수[1,500]
-	M : 도로 개수[1,2500] - 무방향
-	W : 웜홀 개수[1,200] - 유방향, 시작->도착 하나의 경로.
-					도착하면 시작했을때보다 시간이 뒤로감
-	가중치에 음의가중치가 있으므로 다익스트라 불가능.
-	메모리제한도 128MB이므로, 그래프 인접리스트 구현 & 벨만포드 알고리즘 사용
-	시작점 모두 적용해서 확인해야 함.
-*/
-bool Bellman_Ford(Vertex* vertex, int start) {
-	vertex[start].len = 0;
-	for (int test = 1; test < N; test++) {
+
+bool Bellman_Ford(int* vertex, vector<pair<int, int>>* edge) {
+	for (int test = 1; test <= N; test++) {
 		for (int v1 = 1; v1 <= N; v1++) {
-			for (Edge* curEdge = vertex[v1].head; curEdge != nullptr; curEdge = curEdge->next) {
-				vertex[curEdge->v2].len = min((vertex[v1].len + curEdge->len), vertex[curEdge->v2].len);
-				if (vertex[start].len < 0) return true;
+			for (int idx = 0; idx < edge[v1].size(); idx++) {
+				int v2 = edge[v1][idx].first;
+				int weight = edge[v1][idx].second;
+
+				if (vertex[v2] > (vertex[v1] + weight)) {
+					vertex[v2] = vertex[v1] + weight;
+
+					if (test == N) return true;
+				}
 			}
 		}
 	}
+	if(vertex[START] < 0) return true;
 	return false;
 }
