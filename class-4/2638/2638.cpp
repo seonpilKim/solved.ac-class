226012f30cff3e4e50611b9bde951bee085e0e18
seonpilKim <kimsunpil123@naver.com>
2021-02-27 02:43:29 +0900

    [2638] 치즈

--- class-4/2638/2638.cpp
+++ class-4/2638/2638.cpp
@@ -2,17 +2,19 @@
 #include <queue>
 
 #define MAX 101
-#define VISITED 2
-#define AIR 0
-#define CHEESE 1
+#define UNVISITED 0
+#define VISITED 1
+#define INIT 0
+#define AIR -1
 #define MELTING -1
-#define INIT true
+#define CHEESE 1
 
 using namespace std;
 
 int N, M;
-int paper[MAX][MAX];
+pair<int,int> paper[MAX][MAX];
 queue<pair<int, int>> q;
+queue<pair<int, int>> q2;
 
 int getTime();
 
@@ -24,7 +26,9 @@ int main() {
 
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= M; j++) {
-			cin >> paper[i][j];
+			int input;
+			cin >> input;
+			paper[i][j] = make_pair(input, UNVISITED);
 		}
 	}
 	
@@ -35,72 +39,93 @@ int main() {
 
 int getTime() {
 	int time = 0;
-	bool all_melting;
-	queue<pair<int, int>> q2;
-	queue<pair<int, int>> q3;
-	queue<pair<int, int>> q4;
 
 	while (true) {
-		all_melting = INIT;
-		q2.push(make_pair(1, 1));
 
-		while (!q2.empty()) {
-			int x = q2.front().first;
-			int y = q2.front().second;
-			q2.pop();
-			paper[x][y] = VISITED;
-			q3.push(make_pair(x, y));
-
-			if (x > 1 && paper[x - 1][y] <= AIR) q2.push(make_pair(x - 1, y));
-			if (N > x && paper[x + 1][y] <= AIR) q2.push(make_pair(x + 1, y));
-			if (y > 1 && paper[x][y - 1] <= AIR) q2.push(make_pair(x, y - 1));
-			if (N > y && paper[x][y + 1] <= AIR) q2.push(make_pair(x, y + 1));
-		}
-
-		while (!q3.empty()) {
-			int x = q3.front().first;
-			int y = q3.front().second;
-			q3.pop();
-			paper[x][y] = MELTING;
-		}
-
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= M; j++) {
-				if (paper[i][j] == CHEESE) q.push(make_pair(i, j));
-			}
-		}
+		/*
+			Classifing the air outside the cheese and the air inside it
+		*/
+		q.push(make_pair(1, 1));
+		paper[1][1].second = VISITED;
 
 		while (!q.empty()) {
 			int x = q.front().first;
 			int y = q.front().second;
 			q.pop();
-			int contacts = 0;
+			q2.push(make_pair(x, y));
 
-			if (x > 1 && paper[x - 1][y] == MELTING) contacts++;
-			if (N > x && paper[x + 1][y] == MELTING) contacts++;
-			if (y > 1 && paper[x][y - 1] == MELTING) contacts++;
-			if (N > y && paper[x][y + 1] == MELTING) contacts++;
-
-			if (contacts > 1) q4.push(make_pair(x, y));
+			if (x > 1 && paper[x - 1][y].first <= INIT && paper[x - 1][y].second == UNVISITED) {
+				q.push(make_pair(x - 1, y));
+				paper[x - 1][y] = make_pair(AIR, VISITED);
+			}
+			if (x < N && paper[x + 1][y].first <= INIT && paper[x + 1][y].second == UNVISITED) {
+				q.push(make_pair(x + 1, y));
+				paper[x + 1][y] = make_pair(AIR, VISITED);
+			}
+			if (y > 1 && paper[x][y - 1].first <= INIT && paper[x][y - 1].second == UNVISITED) {
+				q.push(make_pair(x, y - 1));
+				paper[x][y - 1] = make_pair(AIR, VISITED);
+			}
+			if (y < M && paper[x][y + 1].first <= INIT && paper[x][y + 1].second == UNVISITED) {
+				q.push(make_pair(x, y + 1));
+				paper[x][y + 1] = make_pair(AIR, VISITED);
+			}
 		}
 
-		while (!q4.empty()) {
-			int x = q4.front().first;
-			int y = q4.front().second;
-			q4.pop();
-			paper[x][y] = MELTING;
+		/* 
+			Initializing the state of visit
+		*/
+		while (!q2.empty()) {
+			int x = q2.front().first;
+			int y = q2.front().second;
+			q2.pop();
+
+			paper[x][y].second = UNVISITED;
+		}
+
+		/* 
+			Find cheeses
+		*/
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= M; j++) {
+				if (paper[i][j].first == CHEESE) q.push(make_pair(i, j));
+			}
+		}
+
+		/* 
+			Find meltable cheeses
+		*/
+		bool isCheese = false;
+		while (!q.empty()) {
+			int contacts = 0;
+			int x = q.front().first;
+			int y = q.front().second;
+			q.pop();
+
+			if (x > 1 && paper[x - 1][y].first == AIR) contacts++;
+			if (x < N && paper[x + 1][y].first == AIR) contacts++;
+			if (y > 1 && paper[x][y - 1].first == AIR) contacts++;
+			if (y < M && paper[x][y + 1].first == AIR) contacts++;
+
+			if (contacts >= 2) q2.push(make_pair(x, y));							
+			else isCheese = true;
 		}
 		time++;
 
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= M; j++) {
-				if (paper[i][j] == CHEESE) {
-					all_melting = false;
-					break;
-				}
-			}
-			if (!all_melting) break;
+		/* 
+			Escape condition from infinity loop
+			: no more cheese
+		*/
+		if (!isCheese) return time;
+
+		/*
+			Melting cheeses
+		*/
+		while (!q2.empty()) {
+			int x = q2.front().first;
+			int y = q2.front().second;
+			q2.pop();
+			paper[x][y].first = MELTING;
 		}
-		if (all_melting) return time;
 	}
 }
