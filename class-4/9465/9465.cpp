dee75b13af925d0f6b26716f56d426e36ad4b76b
seonpilKim <kimsunpil123@naver.com>
2021-03-03 16:22:03 +0900

    [9465] 스티커 : WA

--- class-4/9465/9465.cpp
+++ class-4/9465/9465.cpp
@@ -7,43 +7,49 @@ using namespace std;
 
 int stiker[2][MAX];
 
-int findMax(int);
+int findMax();
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-	
+
 	int T;
 
 	cin >> T;
 
-	while (T--) {
-		int n;
-		cin >> n;
-
-		for (int r = 0; r < 2; r++) {
-			for (int c = 0; c < n; c++) {
-				cin >> stiker[r][c];
-			}
-		}
-
-		cout << findMax(n) << '\n';
-	}
+	while (T--) cout << findMax() << '\n';
 
 	return 0;
 }
 
-int findMax(int n) {
+int input() {
+	int n;
+	cin >> n;
+
+	for (int r = 0; r < 2; r++) {
+		for (int c = 0; c < n; c++) {
+			cin >> stiker[r][c];
+		}
+	}
+
+	return n;
+}
+
+int findMax() {
+	int n = input();
 	int c = 0;
-	int Max = 0;
+
+	if (n == 1) return max(stiker[0][0], stiker[1][0]);
 
 	while (true) {
-		if (c + 2 <= n) {
+		if (c + 2 < n) {
 			stiker[1][c + 2] += max((stiker[0][c]), (stiker[1][c] + stiker[0][c + 1]));
 			stiker[0][c + 2] += max((stiker[1][c]), (stiker[0][c] + stiker[1][c + 1]));
 			c += 2;
+
+			if (c == n - 1) break;
 		}
-		else if (c + 1 == n) {
+		else if (c + 1 == n - 1) {
 			stiker[1][c + 1] += stiker[0][c];
 			stiker[0][c + 1] += stiker[1][c];
 			break;
