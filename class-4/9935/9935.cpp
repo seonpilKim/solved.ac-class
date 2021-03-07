73f318345591c9c90cee2acffaadbd0fc36f354a
seonpilKim <kimsunpil123@naver.com>
2021-03-08 00:00:32 +0900

    [9935] 문자열 폭발

--- class-4/9935/9935.cpp
+++ class-4/9935/9935.cpp
@@ -1,35 +1,49 @@
 #include <iostream>
 #include <string>
-#include <stack>
+#include <vector>
 
 using namespace std;
 
-string str;
-string boomb;
-
 int main() {
-	stack<int> s;
+	string str;
+	string boomb;
+	vector<char> ans;
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
 
 	cin >> str >> boomb;
 
+	int size = str.size();
+	int cont = 0;
+
 	for (int i = 0; i < str.size(); i++) {
-		if (str[i] == boomb[0]) s.push(i);
+		ans.push_back(str[i]);
+
+		if (str[i] == boomb[boomb.size() - 1]) {
+			bool isboomb = true;
+
+			for (int j = 0; j < boomb.size(); j++) {
+				if (ans[ans.size() - 1 - j] != boomb[boomb.size() - 1 - j]) {
+					isboomb = false;
+					break;
+				}
+			}
+
+			if (isboomb) {
+				for (int j = 0; j < boomb.size(); j++) {
+					ans.pop_back();
+				}
+				size -= boomb.size();
+			}
+		}
 	}
 
-	while (!s.empty()) {
-		int idx = s.top(); s.pop();
-		int cnt = 0;
-		for (int i = 0; i < boomb.size(); i++) {
-			if (str[idx + i] == boomb[i]) cnt++;
-		}
-		if (cnt == boomb.size()) {
-			string tmp = str.substr(0, idx);
-			string tmp1 = str.substr(idx + boomb.size());
-			str = tmp + tmp1;
+	if (size) {
+		for (int i = 0; i < size; i++) {
+			cout << ans[i];
 		}
 	}
-
-	if (str.size()) cout << str;
 	else cout << "FRULA";
 
 	return 0;
